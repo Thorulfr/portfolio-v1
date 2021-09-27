0ee372fd25fcaf4ecf650e44d5bae97bdc1573de
Ben H <deepfreespace@gmail.com>
2021-09-27 17:14:27 -0600

    Added scroll-to-top button

---
+++ assets/js/primary-js.js
@@ -0,0 +1,22 @@
+// Get button from HTML
+scrollButton = document.getElementById("scroll-to-top-button");
+
+// Display button once user scrolls
+window.onscroll = function() {showScrollButton()};
+
+// Scroll to top when user clicks button
+function scrollToTop() {
+    // Safari
+    document.body.scrollTop = 0;
+    // Everything else
+    document.documentElement.scrollTop = 0;
+}
+
+// Show/hide button, depending where user is on page
+function showScrollButton() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
